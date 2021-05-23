56b972644382f86b61bbfc88acd9a85e31a204c6
Marek Szyca <malutek1221@gmail.com>
2021-05-23 17:08:04 +0200

    Added ClearDB to project. Try to deploy database on heroku.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,16 @@
 <?php
 
-$servername = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "mtvdancefit";
-$servername = "localhost";
 
-
-$conn = mysqli_connect($servername, $user, $pass, $dbname);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if(!$conn) {
     die( "<script>alert('Connection Failed')</script>");
