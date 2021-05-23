875d730e7358de837008ec1391bd96022c5149fd
Marek Szyca <malutek1221@gmail.com>
2021-05-23 17:27:28 +0200

    step back in database settings

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,9 +3,9 @@
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
-$cleardb_username = $cleardb_url["b97b03f8136a9c"];
-$cleardb_password = $cleardb_url["2436b5a9"];
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
